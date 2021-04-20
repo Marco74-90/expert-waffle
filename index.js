d8822727d942c4af328850fc9c6fca7876c5e1b3
Amy Mangor <aemangor@gmail.com>
2021-04-20 16:33:31 -0400

    like button

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 console.log("Hello Marcus!");
 
-const Base_Url = "http://localhost:3000/quotes" 
+const Base_Url = "http://localhost:3000/quotes/";
 
 fetch(Base_Url)
-.then(resp => resp.json())
-.then(quotes => quotes.forEach(renderQuotes))
+  .then((resp) => resp.json())
+  .then((quotes) => quotes.forEach(renderQuotes));
 
-function renderQuotes(quotes){
-    const button = document.createElement("BUTTON")
-    button.innerText = quotes.id
-    document.getElementById("anime-bar").append(button)
+function renderQuotes(quotes) {
+  const button = document.createElement("BUTTON");
+  button.innerText = quotes.id;
+  document.getElementById("anime-bar").append(button);
 
-    button.addEventListener("click", () => {
-        document.getElementById("name").innerText = quotes.character
-        document.getElementById("quote").innerText = quotes.quote
-        document.getElementById("show").innerText = quotes.anime
-        document.getElementById("image").src = quotes.image
-    })
+  button.addEventListener("click", () => {
+    document.getElementById("name").innerText = quotes.character;
+    document.getElementById("quote").innerText = quotes.quote;
+    document.getElementById("show").innerText = quotes.anime;
+    document.getElementById("image").src = quotes.image;
+    const likes = document.querySelector(".likes");
+    likes.innerText = `Likes: ${quotes.likes}`;
+    likes.addEventListener("click", (event) => {
+      const id = quotes.id;
+      quotes.likes += 1;
+
+      const updatedLikes = {
+        likes: quotes.likes + 1,
+      };
+
+      console.log(event);
+
+      const reqObj = {
+        headers: { "Content-Type": "application/json" },
+        method: "PATCH",
+        body: JSON.stringify(updatedLikes),
+      };
+      fetch(Base_Url + id, reqObj)
+        .then((response) => response.json())
+        .then((updatedLikes) => {
+          likes.innerText = `Likes: ${updatedLikes.likes}`;
+        });
+    });
+  });
 }
