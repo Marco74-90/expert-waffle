38908b86435669367e17f655ad6ee855dd7ae216
marcusdickens <marcus.r.dickens@gmail.com>
2021-04-20 09:47:45 -0400

    Added fetch request and forEach loop to create buttons and append to the DOM

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("Hello Marcus!");
+
+const Base_Url = "http://localhost:3000/quotes" 
+
+fetch(Base_Url)
+.then(resp => resp.json())
+.then(quotes => quotes.forEach(renderQuotes))
+
+function renderQuotes(quotes){
+    const button = document.createElement("BUTTON")
+    button.innerText = quotes.id
+    document.getElementById("anime-bar").append(button)
+
+    button.addEventListener("click", () => {
+        document.getElementById("name").innerText = quotes.character
+        document.getElementById("quote").innerText = quotes.quote
+        document.getElementById("show").innerText = quotes.anime
+    })
+}
